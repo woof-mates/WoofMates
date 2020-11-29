0a2ec8ff205cef89121e2699f8147cf6b516f56f
Fu Goto <fu.goto@gmail.com>
2020-11-29 16:24:05 -0500

    unseen users

--- server/api/match.js
+++ server/api/match.js
@@ -13,21 +13,25 @@ router.get('/:userId', async(req,res,next) => {
         });
         // send one match at a time so algo can update with each decision by user
         // sending 1st element in matches array for now, but this would be sorted based on algorithm
-        if(matchesAlreadyLikedUser) {
+        if(matchesAlreadyLikedUser.length) {
             const matchToSendId = matchesAlreadyLikedUser[0].userId;
             const matchToSend = await User.findByPk(matchToSendId, { include: [Dog] })
             res.send(matchToSend)
         }
+        // if there are no matches that have already liked this user...
         else {
-            const unseenMatches = await User.findAll({
+            const seenMatches = await Relationship.findAll({
                 where: {
-                    '$Relationship.userId$': userId
-                },
-                include: [{
-                    model: Relationship,
-                    required: false
-                }],
+                    userId
+                }
+            });
+            let seenMatchesId = []
+            seenMatches.forEach(match => seenMatchesId.push(match.matchId))
+            const allUsers = await User.findAll({ include: Dog });
+            const unseenMatches = allUsers.filter(user => {
+                return (!seenMatchesId.includes(user.id) && user.id !== userId*1)
             })
+            // same as above, sending first of array for now
             res.send(unseenMatches[0])
         }
     } catch(err) { next(err) }
