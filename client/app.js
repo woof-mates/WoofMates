f1179049e617af20029f3f7bbfab171ffbbbf48f
Lawrence <lkuang93@gmail.com>
2020-12-07 13:12:05 -0500

    removed test video from navbar

--- client/app.js
+++ client/app.js
@@ -23,7 +23,6 @@ class App extends React.Component {
                         <li><Link to='/chat'>Chat</Link></li>
                         <li><Link to='/login'>Login</Link></li>
                         <li><Link to='/match'>Find Your Match</Link></li>
-                        <li><Link to='/video'>Video Test</Link></li>
                     </ul>
                     <Route path='/' exact />
                     <Route path='/signUp' exact component={Registration}/>
@@ -31,7 +30,6 @@ class App extends React.Component {
                     <Route path='/chat' exact component={Chatrooms}/>
                     <Route path='/login' exact component={Login}/>
                     <Route path='/match' exact component={Match}/>
-                    <Route path='/video' exact component={VideoChatContainer}/>
                 </div>
             </Router>
         );
