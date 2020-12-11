58cbf9436e84a67c90c895deb3ae4c2ad48467f2
Lawrence <lkuang93@gmail.com>
2020-12-10 21:34:53 -0500

    updated mapquest

--- client/components/chat.js
+++ client/components/chat.js
@@ -1,25 +1,8 @@
 import React from 'react';
 
-<<<<<<< HEAD
-import firebase from 'firebase';
 import VideoChatContainer from './VideoChat/VideoChatContainer';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDZUsDKQ79fy2TRZJWfmiprYqHUdizGLlo",
-    authDomain: "dog-chat-74ccf.firebaseapp.com",
-    databaseURL: "https://dog-chat-74ccf.firebaseio.com",
-    projectId: "dog-chat-74ccf",
-    storageBucket: "dog-chat-74ccf.appspot.com",
-    messagingSenderId: "391829997647",
-    appId: "1:391829997647:web:d5de6332d455f8c3ab823a"
-};
-
-firebase.initializeApp(firebaseConfig);
-
-const db = firebase.database();
-=======
-import firebaseDB from './Firebase'
->>>>>>> 185fc9a46e704d049969b9eae5a68e985a4d15f7
+import firebaseDB from './Firebase';
 
 class Chat extends React.Component {
     constructor(props) {
