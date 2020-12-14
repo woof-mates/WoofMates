f4a22492ca7e8cd24b561178b7ef7767b92f7557
Lawrence <lkuang93@gmail.com>
2020-12-14 12:00:15 -0500

    fixed .env file for heroku deployment

--- client/components/Firebase.js
+++ client/components/Firebase.js
@@ -3,7 +3,7 @@ import firebase from 'firebase';
 const firebaseConfig = {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
+    databaseURL: process.env.FBDATABASE_URL,
     projectId: process.env.PROJECT_ID,
     storageBucket: process.env.STORAGE_BUCKET,
     messagingSenderId: process.env.MESSAGING_SENDER_ID,
