71faf4a2ec5f7b47c72d4003951ca70264e91377
Lawrence <lkuang93@gmail.com>
2020-12-14 15:58:24 -0500

    firebase update

--- client/components/Firebase.js
+++ client/components/Firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FBDATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
+    apiKey: 'AIzaSyDZUsDKQ79fy2TRZJWfmiprYqHUdizGLlo',
+    authDomain: 'dog-chat-74ccf.firebaseapp.com',
+    databaseURL: 'https://dog-chat-74ccf.firebaseio.com',
+    projectId: 'dog-chat-74ccf',
+    storageBucket: 'dog-chat-74ccf.appspot.com',
+    messagingSenderId: '391829997647',
+    appId: '1:391829997647:web:d5de6332d455f8c3ab823a'
 };
 
 firebase.initializeApp(firebaseConfig);
