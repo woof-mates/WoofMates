269a2192ae58006bc35f50e0aa0cbfdd7c30d5c5
Lawrence <lkuang93@gmail.com>
2020-12-15 19:36:38 -0500

    removed welcome message

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,43 +1,12 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import HomeLandingPage from './HomeLandingPage';
 
-class Home extends Component {
-    constructor(props){
-        super(props);
-    }
-
-    componentDidMount() {
-      const {user} = this.props
-    }
-
-    render(){
-      const {user} = this.props
-        if (user.firstName) {
-          return (
-            <div id="homePageContainer">
-              <HomeLandingPage/>
-            </div>
-          )
-        }
-        else {
-          return (
-            <div id="homePageContainer">
-                <HomeLandingPage/>
-            </div>
-            )
-          }
-    }
+const Home = () => {
+  return (
+    <div id="homePageContainer">
+      <HomeLandingPage/>
+    </div>
+  )
 }
 
-const mapStateToProps = (state) => {
-    return {
-        user: state.user
-    }
-};
-
-const mapDispatchToProps = (dispatch) => ({
-    //login: (loginInfo) => dispatch(login(loginInfo)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default (Home);
--- client/components/HomeLandingPage.js
+++ client/components/HomeLandingPage.js
@@ -1,35 +1,22 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import Slideshow from './Slideshow';
 import GetStarted from './GetStarted';
 
-class HomeLandingPage extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        const { user } = this.props
-        return (
-            <div className='slideshowContainer'>
-                { user.id ? <div className='welcome'>Welcome, {user.firstName}!</div> : ''}
-                <div className='slideTitle'>
-                    <h2>Discover Dog Owners and Their Best-Friend</h2>
-                    <img width={50} height={50} src={'/images/pawprint.jpg'} />
-                </div>
-                <div className='slideshow'>
-                    <Slideshow/>
-                </div>
-                <div className='getStarted'>
-                    <GetStarted/>
-                </div>
+const HomeLandingPage = () => {
+    return (
+        <div className='slideshowContainer'>
+            <div className='slideTitle'>
+                <h2>Discover Dog Owners and Their Best-Friend</h2>
+                <img width={50} height={50} src={'/images/pawprint.jpg'} />
             </div>
-        )
-    };
-};
+            <div className='slideshow'>
+                <Slideshow/>
+            </div>
+            <div className='getStarted'>
+                <GetStarted/>
+            </div>
+        </div>
+    )
+}
 
-const mapStateToProps = (state) => ({
-    user: state.user,
-});
-
-export default connect(mapStateToProps)(HomeLandingPage);
+export default HomeLandingPage
