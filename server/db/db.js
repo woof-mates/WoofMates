f5299ed4576db291a584391785f6a62452b3dea5
Fu Goto <fu.goto@gmail.com>
2020-12-15 22:25:59 -0500

    deploy

--- server/db/db.js
+++ server/db/db.js
@@ -1,4 +1,6 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/woofmates", {logging:false});
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/woofmates", {logging: false, ssl: {
+  rejectUnauthorized: false
+}});
 
 module.exports = db;
