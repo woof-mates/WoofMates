e3312261a320ade7f108bccc0767a8f960eecbd7
Fu Goto <fu.goto@gmail.com>
2020-12-20 21:13:11 -0500

    updates

--- server/matchAlg/match.js
+++ server/matchAlg/match.js
@@ -1,17 +1,7 @@
 const {scoreActivity, scorePreferences} = require('./score')
-const chalk = require('chalk')
 const {getRandomInt} = require('../../utils/mathFuncs')
-// const User = require('../db/models/User')
-//  const { User, Preference, Dog, Userpref} = require('../db')
 
 const findMatch = (currUser, users) => {
-// const findMatch = async (currUserId, user2Id) => {
-
-  //use score to return dog with highest score
-  // const currUser = await User.findByPk(currUserId, { include: [Preference, Userpref] })
-  // const possibleMatch = await User.findByPk(user2Id, {include: [Dog]})
-  // console.log(chalk.red('Possible Match'))
-  // console.log(possibleMatch)
   let highestScore = -1;
   let bestMatch = {};
   let currScore = 0;
@@ -32,16 +22,12 @@ const findMatch = (currUser, users) => {
     }
     activityScore = scoreActivity(possibleMatch, currUser)
     currScore = activityScore + statedPrefScore;
-    // console.log(`activity score: ${activityScore}`)
-    // console.log(`statedPrefScore: ${statedPrefScore}`)
     if (currScore > highestScore){
       highestScore = currScore;
       bestMatch = possibleMatch;
     }
-    // console.log(`currScore ${currScore}`)
   }
   return bestMatch;
 }
 
-// findMatch(1, 2);
 module.exports = {findMatch}
