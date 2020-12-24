f0aaaf9ee7d34d1f7c8b82bd2f13922c7aab942e
Katerina Solovyova <yekaterina.solovyova@gmail.com>
2020-12-25 01:27:31 +0300

    added form styling to leave a testimonial on testimonial component.

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -39,7 +39,7 @@ class Navbar extends React.Component {
       return (
         <div id="NavbarContainer">
           <div id="NavbarTitleContainer">
-            <Link id="NavbarTitleLink" to="/">Woofmates</Link>
+            <Link id="NavbarTitleLink" to="/">WoofMates</Link>
           </div>
           <div id="NavbarLogoContainer">
             <img id="NavbarLogoPic" src="https://cdn.shopify.com/s/files/1/0667/0685/files/tintin_and_snowy_by_genus_rattus-d4jxohy_large.png?9525775607535621580"></img>
--- client/components/Testimonials.js
+++ client/components/Testimonials.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import axios from 'axios';
 import { getTestimonials } from '../store/testimonials'
 import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select'
 
 
 class Testimonials extends React.Component {
@@ -16,14 +17,21 @@ class Testimonials extends React.Component {
   async componentDidMount () {
     this.props.getTestimonials()
     // const testimonials = (await axios.get('/api/testimonials')).data
-    // this.setState ({
-    //   testimonials
-    // })
+    this.setState ({
+      reviewTitle: '',
+      numberOfStars: '',
+      reviewBody: '',
+      userId: this.props.user.id
+    })
     this.createReview = this.createReview.bind(this)
   }
 
   createReview (ev) {
-    console.log(ev.target)
+    this.setState({
+      [ev.target.name]: ev.target.value,
+      userId: this.props.user.id
+    })
+    console.log(this.state)
   }
 
   render() {
@@ -42,17 +50,33 @@ class Testimonials extends React.Component {
         <div id="testimonialsTitle">
           <h3>Testimonials</h3>
             <div id="writeTestimonialContainer">
+              <h4>Write your own review of WoofMates here!</h4>
               <TextField
                 variant="filled"
                 margin="normal"
                 required
-                fullWidth
+                // fullWidth
                 id="review-title"
                 label="Review Title"
                 name="reviewTitle"
                 autoComplete="reviewTitle"
                 className="reviewTitle" onChange={this.createReview}
-                />
+                /><br></br>
+              <TextField
+              variant="filled"
+              margin="normal"
+              required
+              multiline
+              // fullWidth
+              id="review-body"
+              label="Review Body"
+              name="reviewBody"
+              autoComplete="reviewBody"
+              className="reviewBody" onChange={this.createReview}
+              />
+              <Select
+              
+              />
             </div>
             <div id="testimonialsContainer">
               {testimonials.map(testimonial => {return (<div id="singleTestimonial">{testimonial.reviewTitle}</div>)})}
