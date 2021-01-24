9d2e902934aa7bf1ecfb4d645ff3bb93b71df662
Katerina Solovyova <yekaterina.solovyova@gmail.com>
2021-01-24 12:52:30 -0500

    added html parser to convert start ratings to html dynamically

--- client/components/Testimonials/TestimonialsContainer.js
+++ client/components/Testimonials/TestimonialsContainer.js
@@ -1,15 +1,45 @@
 import React from "react";
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 import { connect } from 'react-redux'
 import { getTestimonials } from '../../store/testimonials'
 
 class TestimonialsContainer extends React.Component {
   constructor(props) {
     super(props)
+    this.generateStars = this.generateStars.bind(this)
   }
 
   async componentDidMount () {
     this.props.getTestimonials()
   }
+
+  generateStars(num) {
+    let starStringHTML = ''
+    for (let i=0; i<num; i++) {
+      starStringHTML += '&#9733';
+    }
+
+    console.log(starStringHTML)
+    // var $ = $( "#testimonialStarRating"),
+    //console.log(document.getElementById('testimonialStarRating').innerHTML)
+
+    // // var parser = new DOMParser();
+    // // var doc = parser.parseFromString(starStringHTML, 'text/html');
+    // console.log(starStringHTML)
+
+    //document.getElementById("testimonialStarRating").innerHTML += starStringHTML
+    //document.getElementById('testimonialStarRating').innerHTML='<div>'+starStringHTML+'</div>';
+    return (
+      // doc.body
+      ReactHtmlParser(starStringHTML)
+      // $('#testimonialStarRating').html(starStringHTML)
+    )
+    // for (let i = 0; i < num; i++) {
+    //   console.log(document.getElementById('testimonialStarRating'))
+    //   document.getElementById('testimonialStarRating').innerHTML += '&#9733;';
+    // }
+  }
+
 
   render() {
     const {testimonials} = this.props
@@ -20,6 +50,8 @@ class TestimonialsContainer extends React.Component {
         <div id="singleTestimonial">
           <b>{testimonial.reviewTitle}</b>
           <br></br>
+          {/* <div id="testimonialStarRating">&#9733;&#9733;&#9733;&#9733;&#9733;</div> */}
+          <div id="testimonialStarRating">{this.generateStars(testimonial.numberOfStars)}</div>
           {testimonial.reviewBody}
           <br></br>
           <i>{testimonial.user.firstName}</i>
